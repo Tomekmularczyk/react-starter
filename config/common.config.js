48ca9aeeb54bac0c112e71c74e4d7f0542a35eda
TomekMularczyk <tomasz.mularczyk@risendot.com>
2017-08-21 09:11:41 +0200

    - chunk fileName for dynamic imports

--- config/common.config.js
+++ config/common.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist/public/'),
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     publicPath: '/',
   },
 
