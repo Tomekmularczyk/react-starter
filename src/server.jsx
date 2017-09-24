bc21b4888432d71c37aea1f3e3612b861260edd3
TomekMularczyk <tomasz.mularczyk@risendot.com>
2017-09-24 18:10:28 +0200

    reverting bad changes
    ;

--- src/server.jsx
+++ src/server.jsx
@@ -41,8 +41,7 @@ server.get('*', (req, res) => {
   }
 });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-server.listen(server_port, server_ip_address, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log(`Production Express server running at localhost:${PORT}`);
 });
