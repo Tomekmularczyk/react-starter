d61c33d5f3b0689f46ed94a71b7f246e8e37bacd
TomekMularczyk <tomasz.mularczyk@risendot.com>
2017-09-25 18:25:00 +0200

    - allow to pass IP to server.js

--- src/server.jsx
+++ src/server.jsx
@@ -42,6 +42,7 @@ server.get('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
+const IP = process.env.IP || '0.0.0.0';
+server.listen(PORT, IP, () => {
   console.log(`Production Express server running at localhost:${PORT}`);
 });
