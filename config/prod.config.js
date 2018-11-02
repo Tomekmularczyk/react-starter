1efb89843cfd8d4dfb71e09c43080e103c77b4a0
Tomasz Mularczyk <tomasz.mularczyk@risendot.com>
2018-11-02 14:04:10 +0100

    add @babel/runtime

--- config/prod.config.js
+++ config/prod.config.js
@@ -3,12 +3,14 @@ const merge = require("webpack-merge");
 const PATHS = require("./paths");
 const parts = require("./webpack.parts");
 
+const vendor = ["@babel/polyfill", "react", "react-dom"];
+
 const productionConfig = merge([
   parts.setProductionMode(),
   parts.setEntries({
-    client: ["@babel/polyfill", PATHS.clientBundleEntry],
+    client: ["@babel/polyfill", PATHS.clientBundleEntry]
   }),
-  parts.createVendorChunk(["@babel/polyfill|react|react-dom"]),
+  parts.createVendorChunk(vendor),
   parts.setOutput(PATHS.mainOutputDirectory, true),
   parts.cleanDirectory(PATHS.mainOutputDirectory, PATHS.root),
   parts.resolveDependencies(),
