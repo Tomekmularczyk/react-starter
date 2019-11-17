e6b77e5f269c53254b5dfad3788256573e7d4e20
Tomek Mularczyk <tomaszmularczyk89@gmail.com>
2019-11-17 15:56:07 +0100

    create script for running the config

---
+++ config/run.js
@@ -0,0 +1,36 @@
+/**
+ * Available commands:
+ * "node run.js -dev" - runs app in development mode
+ * "node run.js -build" - builds the app in production mode
+ */
+
+const spawn = require("cross-spawn");
+
+const webpackFile = require.resolve(`./webpack.run.js`);
+
+const [task] = process.argv.slice(2);
+
+switch (task) {
+  case "-dev": {
+    result = spawn.sync(
+      "webpack-dev-server",
+      ["--config", webpackFile, "--progress", "--env.development"],
+      { stdio: "inherit" }
+    );
+    break;
+  }
+  case "-build": {
+    result = spawn.sync(
+      "webpack",
+      ["--config", webpackFile, "--progress", "--env.production"],
+      { stdio: "inherit" }
+    );
+    break;
+  }
+}
+
+if (result.signal) {
+  process.exit(1);
+}
+
+process.exit(result.status);
